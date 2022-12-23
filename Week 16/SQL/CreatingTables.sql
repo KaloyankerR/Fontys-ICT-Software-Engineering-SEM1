c57510dc8ffa1b4013244899e594080175a64773
KaloyankerR <kaloyankulov2003kk@gmail.com>
2022-12-23 19:22:32 +0100

    SQL queries for the individual assignment

--- Week 16/SQL/CreatingTables.sql
+++ Week 16/SQL/CreatingTables.sql
@@ -1,22 +1,21 @@
+-- Creating Car table
 --create table Car (
---	CarId int primary key,
---	Brand varchar(50),
---	Model varchar(50),
---	YearProduced int,
---	Sold bit,
---	BasePrice float
+--	CarId int primary key identity(1, 1),
+--	Brand varchar(50) not null,
+--	Model varchar(50) not null,
+--	YearProduced int not null,
+--	BasePrice float not null,
+--	Sold bit
 --);
 
---create table Customer (
---	CustomerId int primary key,
---	FirstName varchar(50),
---	LastName varchar(50),
---	PhoneNo int,
+-- Creating Customer table
+--create table Customer(
+--	CustomerId int primary key identity(1, 1),
+--	Name varchar(80) not null,
+--	PhoneNo int not null,
 --	Email varchar(50),
---	CustomerAddress varchar(50),
---	Zipcode varchar(50),
---	City varchar (25),
---	CarId int foreign key references Car(CarId) 
+--	CustomerAddress varchar(50) not null,
+--	ZipcodeAndCity varchar(50) not null 
 --);
 
 --create table Sales (
@@ -26,6 +25,21 @@
 --	PurchaseDate date
 --);
 
-select *
-from Sales
+-- Inserting into Car table
+-- insert into Car(Brand, Model, YearProduced, BasePrice)
+-- values('marka', 'model', 2003, 25739)
+
+-- Inserting into Customer table
+--insert into Customer(Name, PhoneNo, Email, CustomerAddress, ZipcodeAndCity)
+--values('Kaloyan Kulov', 359, 'email.com', 'Boschdijk', '1421 Sofia')
+
+--select *
+--from Customer
+
+--delete from Car
+--where CarId = 1	
+
+--drop table Sales
+--drop table Customer
+--drop table Car
 
